e432a5e127edff5742e20d02b0956c849eb9aa11
cfarthingale <trose@0x00000.com>
2018-09-21 20:51:31 -0700

    Update conky-robinhood.sh

--- conky-robinhood.sh
+++ conky-robinhood.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 # A poorly commented RobinHood API script for use with Conky.
-authtoken=(your_auth_token)
+authtoken=(your_api_token)
 apiurl=(https://api.robinhood.com)
 totaldeposits=(1000.00)
 totalwithdrawals=(0)
-#opencheck=$(curl -s $apiurl/markets/|jq -r .results[3].todays_hours|xargs curl -s|jq -r '.is_open')
+curl -s $apiurl/portfolios/ -H "Accept: application/json" -H "Authorization: Token $authtoken"|jq '.' > /dev/shm/data.tmp
+curl -s $apiurl/accounts/ -H "Accept: application/json" -H "Authorization: Token $authtoken"|jq '.' >> /dev/shm/data.tmp
+curl -s $apiurl/positions/ -H "Accept: application/json" -H "Authorization: Token $authtoken"|jq '.' > /dev/shm/positions.tmp
 hour=$(date "+%k")
-opencheck=$((( 5 <= hour && hour < 12 )) && echo true || echo false)
+opencheck=$((( 6 <= hour && hour < 13 )) && echo true || echo false)
+#opencheck=$(curl -s $apiurl/markets/|jq -r .results[3].todays_hours|xargs curl -s|jq -r '.is_open')
 if [[ $opencheck == "true" ]];then
-	equity=$(curl -s $apiurl/portfolios/ -H "Accept: application/json" -H "Authorization: Token $authtoken"|jq -r '.results[].equity')
+	equity=$(cat /dev/shm/data.tmp |jq -r '.results[0].equity // empty')
+	marketvalue=$(cat /dev/shm/data.tmp |jq -r '.results[0].market_value // empty')
 else
-	equity=$(curl -s $apiurl/portfolios/ -H "Accept: application/json" -H "Authorization: Token $authtoken"|jq -r '.results[].extended_hours_equity')
+	equity=$(cat /dev/shm/data.tmp |jq -r '.results[0].extended_hours_equity // empty')
+	marketvalue=$(cat /dev/shm/data.tmp |jq -r '.results[0].extended_hours_market_value // empty')
 fi
-marketvalue=$(curl -s $apiurl/portfolios/ -H "Accept: application/json" -H "Authorization: Token $authtoken"|jq -r '.results[].market_value')
-buyingpower=$(curl -s $apiurl/accounts/ -H "Accept: application/json" -H "Authorization: Token $authtoken"|jq -r '.results[].buying_power')
-unsettledfunds=$(curl -s $apiurl/accounts/ -H "Accept: application/json" -H "Authorization: Token $authtoken"|jq -r '.results[].unsettled_funds')
-unsettleddebit=$(curl -s $apiurl/accounts/ -H "Accept: application/json" -H "Authorization: Token $authtoken"|jq -r '.results[].unsettled_debit')
-uncleareddeposits=$(curl -s $apiurl/accounts/ -H "Accept: application/json" -H "Authorization: Token $authtoken"|jq -r '.results[].uncleared_deposits')
-withdrawable=$(curl -s $apiurl/accounts/ -H "Accept: application/json" -H "Authorization: Token $authtoken"|jq -r '.results[].cash_available_for_withdrawal')
-cash=$(curl -s $apiurl/accounts/ -H "Accept: application/json" -H "Authorization: Token $authtoken"|jq -r '.results[].cash'|cut -c1-7)
-positions=$(curl -s $apiurl/positions/ -H "Accept: application/json" -H "Authorization: Token $authtoken")
+buyingpower=$(cat /dev/shm/data.tmp |jq -r '.results[0].buying_power // empty')
+unsettledfunds=$(cat /dev/shm/data.tmp |jq -r '.results[0].unsettled_funds // empty')
+unsettleddebit=$(cat /dev/shm/data.tmp |jq -r '.results[0].unsettled_debit // empty')
+uncleareddeposits=$(cat /dev/shm/data.tmp |jq -r '.results[0].uncleared_deposits // empty')
+withdrawable=$(cat /dev/shm/data.tmp |jq -r '.results[0].cash_available_for_withdrawal // empty')
+cash=$(cat /dev/shm/data.tmp |jq -r '.results[0].cash // empty'|cut -c1-7)
+positions=$(cat /dev/shm/positions.tmp)
+positionsc=$(echo $(echo "$positions"|grep url|wc -l)-1|bc)
 totalprofit=$(echo "${equity}-${totaldeposits}"| bc)
 totalprofitc=$(echo "${totalprofit}"|cut -c1)
 totalprofitp=$(echo "${totalprofit} ${totaldeposits}"|awk '{print $1/$2*100}')
-#
-#Section A: Gathers various account/status information from api.robinhood.com
-echo "+ Robinhood Account 0 - \${time %d %b %Y %H:%M:%S} - TESTING"
+echo "+ Robinhood Account 0 - \${time %d %b %Y %H:%M:%S} - PRODUCTION"
 echo " +"
+#
 #Check if market is open
 if [[ $opencheck == "true" ]];then
 	echo " | + Markets are  : \${color green}OPEN\${color}"
@@ -65,9 +69,8 @@ echo " | + Withdrawn    : $totalwithdrawals"
 echo " | + Withdrawable : $withdrawable"
 echo " +"
 #
-#Section B: Gathers and prints information about owned $positions. The next line has to be adjusted to the number of stocks you own (i.e. 0 1 2 3 4 5 if you own 6 stocks.). Any suggestings for a better method here gratefully accepted.
-for e in {0..9}; do
-	symbol=$(echo "$positions"|jq -r .results[$e].instrument|xargs curl -s|jq -r '.symbol')
+for e in $(seq 0 $positionsc); do
+	symbol=$(echo "$positions"|jq .results[$e].instrument|xargs curl -s|jq -r '.symbol')
 	quantity=$(echo "$positions"|jq -r .results[$e].quantity|cut -c1-5)
 	paid=$(echo "$positions"|jq -r .results[$e].average_buy_price)
 	spentusd=$(echo "${quantity}*${paid}"| bc)
@@ -77,11 +80,15 @@ for e in {0..9}; do
 	else
 		price=$(echo "$positions"|jq -r .results[$e].instrument|xargs curl -s|jq -r '.quote'|xargs curl -s|jq -r '.last_extended_hours_trade_price'|cut -c1-5)
 	fi
+	if [[ $price == "null" ]];then
+		price=$(echo "$positions"|jq -r .results[$e].instrument|xargs curl -s|jq -r '.quote'|xargs curl -s|jq -r '.last_trade_price'|cut -c1-5)
+	fi
+#	lastclose=$(echo "$positions"|jq -r .results[$e].instrument|xargs curl -s|jq -r '.quote'|xargs curl -s|jq -r '.previous_close'|cut -c1-5) # Price at previous close
+#	lastclosep=$(echo ${price} ${lastclose}|awk '{print 100*$1/$2-100}'|cut -c1-5)
 	equity=$(echo "${price}*${quantity}"| bc)
 	return=$(echo "${equity}-${spentusd}"| bc)
 	returnc=$(echo "${return}"|cut -c1)
 	returnp=$(echo ${return} ${spentusd}|awk '{print $1/$2*100}'|cut -c1-5)
-	#
 	echo "+ $symbol +$quantity shares"
 	echo " +"
 	echo " | + Price  : $price"
@@ -93,11 +100,12 @@ for e in {0..9}; do
 	else
 		echo " | + Return : \${color green}"${return} +${returnp}\%"\${color}"
 	fi
-	#ALERTS
+#ALERTS
 	if (( $(echo "$returnp > 10" |bc) )); then
-		echo "\${color orange} | + ALERT  : Above +9% return = Sell?\${color}"
-		echo "Alert, $symbol up $returnp%, Sell now to lock in $return dollars"|espeak
+		echo "\${color orange} | + ALERT  : Above +10% return = Sell?\${color}"
+#		echo "Alert, $symbol up $returnp%, Sell now to lock in $return dollars"|espeak
 	fi
 echo " +"
 done
 echo "+"
+exit 0
